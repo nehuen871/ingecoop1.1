c92aa856abd101f3329e334bb339ae37c49ef7b1
nehuen <nehuennavarro@gmai.com>
2019-11-12 10:14:59 -0300

    cambios

--- client/src/components/SideBar.js
+++ client/src/components/SideBar.js
@@ -13,37 +13,37 @@ export default class SideBar extends Component {
                 <li className="header">Navigation</li>
                 <li>
                     <Link to="/">
-                        <i className="fa fa-home" aria-hidden="true"></i> Proyecto
+                        <i className="fa fa-home" aria-hidden="true"></i>Listado de Proyecto
                     </Link>
                 </li>
                 <li>
                     <Link to="/Cliente">
-                        <i className="fa fa-tachometer" aria-hidden="true"></i> Cliente
+                        <i className="fa fa-tachometer" aria-hidden="true"></i>Listado de Cliente
                     </Link>
                 </li>
                 <li>
                     <Link to="/Cotizacion">
-                        <i className="fa fa-users" aria-hidden="true"></i> Cotizacion
+                        <i className="fa fa-users" aria-hidden="true"></i>Listado de Cotizacion
                     </Link>
                 </li>
                 <li>
                     <Link to="/Control">
-                        <i className="fa fa-cog" aria-hidden="true"></i> Control
+                        <i className="fa fa-cog" aria-hidden="true"></i>Listado de Control
                     </Link>
                 </li>
                 <li>
                     <Link to="/Certificacion">
-                        <i className="fa fa-info-circle" aria-hidden="true"></i> Certificacion
+                        <i className="fa fa-info-circle" aria-hidden="true"></i>Listado de Certificacion
                     </Link>
                 </li>
                 <li>
                     <Link to="/DatosCotizacion">
-                        <i className="fa fa-info-circle" aria-hidden="true"></i> Datos de Cotizacion
+                        <i className="fa fa-info-circle" aria-hidden="true"></i>Listado de Datos de Cotizacion
                     </Link>
                 </li>
                 <li>
                     <Link to="/DatosControl">
-                        <i className="fa fa-info-circle" aria-hidden="true"></i> Datos de Control
+                        <i className="fa fa-info-circle" aria-hidden="true"></i>Listado de Datos de Control
                     </Link>
                 </li>
                 <li>
