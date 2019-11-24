4b4691af423366b6d50515ffd4fe1832f17c0cb3
agus921 <agustinnavarro8@gmail.com>
2019-11-24 16:18:09 -0300

    test

--- client/src/components/Tree.js
+++ client/src/components/Tree.js
@@ -5,15 +5,19 @@ import 'react-sortable-tree/style.css'; // This only needs to be imported once i
 export default class Tree extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
+      dataTest: 0,
       treeData: [
         { title: 'Chicken', children: [{ title: 'Egg' }] },
         { title: 'Fish', children: [{ title: 'fingerline'}] }
       ],
     };
+    this.changeLinkValue = this.changeLinkValue.bind(this);
   }
-
+  changeLinkValue(){
+    this.props.changeLink(this.state.dataTest);
+    console.log(this.state.dataTest);
+  }
   render() {
     return (
       <div style={{ height: 400 }}>
@@ -21,6 +25,7 @@ export default class Tree extends Component {
           treeData={this.state.treeData}
           onChange={treeData => this.setState({ treeData })}
         />
+        <button onClick={this.changeLinkValue} className="btn btn-primary">Tests</button>
       </div>
     );
   }
--- client/src/components/form/Search.js
+++ client/src/components/form/Search.js
@@ -6,14 +6,15 @@ import Tree from "../Tree";
 export default class NameForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
-
+    this.state = {
+      value: ''
+    };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
+  handleChange(values) {
+    this.setState({value: values});
   }
 
   handleSubmit(event) {
@@ -31,7 +32,7 @@ export default class NameForm extends React.Component {
         </label>
         <input type="submit" value="Submit" />
       </form>
-      <Tree />
+      <Tree changeLink={this.handleChange}/>
       </div>
     );
   }
