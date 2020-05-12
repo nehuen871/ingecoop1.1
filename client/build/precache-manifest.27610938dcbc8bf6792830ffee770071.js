15e91b564746f4e6323cd9194694c7e2d07515c7
nehuen <nehuennavarro@gmai.com>
2020-05-12 15:44:10 -0300

    test2

---
+++ client/build/precache-manifest.27610938dcbc8bf6792830ffee770071.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "549fb185e9e2d63b067f13a2e754431e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6048757a89bbf4771f00",
+    "url": "/static/css/2.dd873368.chunk.css"
+  },
+  {
+    "revision": "1363178e588c2d70d191",
+    "url": "/static/css/main.167b714e.chunk.css"
+  },
+  {
+    "revision": "6048757a89bbf4771f00",
+    "url": "/static/js/2.5de5e1ec.chunk.js"
+  },
+  {
+    "revision": "4a11a3b64360f77bd03bc13a7854010b",
+    "url": "/static/js/2.5de5e1ec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1363178e588c2d70d191",
+    "url": "/static/js/main.3211a4b9.chunk.js"
+  },
+  {
+    "revision": "0803fe97dad94b88e801",
+    "url": "/static/js/runtime-main.cb7a5419.js"
+  }
+]);
