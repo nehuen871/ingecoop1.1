310f4be93a811807d4dd2044e95e210b17dedea8
nehuen <nehuennavarro@gmail.com>
2020-09-14 17:02:11 -0300

    test

--- db/procedureRecotizacion.sql
+++ db/procedureRecotizacion.sql
@@ -14,6 +14,7 @@ BEGIN
     DECLARE _idCotizacion INT DEFAULT 0;
     DECLARE _titulo_cotiazacionNew VARCHAR(255);
     DECLARE _a INT DEFAULT 0;
+    DECLARE _b INT DEFAULT 0;
     DECLARE _numero_doc VARCHAR(255);
     DECLARE _cliente_id INT DEFAULT 0;
     DECLARE _numeroRecotizacion INT DEFAULT 0;
@@ -28,6 +29,10 @@ BEGIN
     DECLARE _viatico BOOLEAN DEFAULT 0;
     DECLARE _numero_documento INT DEFAULT 0;
     DECLARE _list_docs_id INT DEFAULT 0;
+    DECLARE _datosControid INT DEFAULT 0;
+    DECLARE _datosCertificacionid INT DEFAULT 0;
+    DECLARE _datosControidOld INT DEFAULT 0;
+    DECLARE _datosCertificacionidOld INT DEFAULT 0;
     DECLARE _fecha DATE DEFAULT NULL;
     
     SELECT fecha,titulo_cotiazacion,numero_doc,cliente_id,codigo_unificador INTO _fecha,_titulo_cotiazacionNew,_numero_doc,_cliente_id,_codigo_unificador FROM cotizacion WHERE id = _id;
@@ -36,6 +41,36 @@ BEGIN
     INSERT INTO cotizacion (fecha,titulo_cotiazacion,numero_doc,cliente_id,codigo_unificador)
     VALUES (_fecha,_titulo_cotiazacionNew,_numero_doc,_cliente_id,_codigo_unificador);
     SET _idCotizacion = LAST_INSERT_ID();
+    
+    simple_loop: LOOP
+    SELECT id,control_cotizacion_id INTO _datosControid,_datosControidOld FROM datosControl WHERE control_cotizacion_id = _id LIMIT 0,1;
+    SELECT id,certificacion_control_cotizacion_id INTO _datosCertificacionid,_datosCertificacionidOld FROM datosCertificacion WHERE certificacion_control_cotizacion_id = _id LIMIT 0,1;
+
+    UPDATE datosCotizacion
+    SET
+      certificacion_control_cotizacion_id = _idCotizacion
+    WHERE id = _datosCertificacionid;
+
+    UPDATE datosControl
+    SET
+      control_cotizacion_id = _idCotizacion
+    WHERE id = _datosControid;
+
+    SET _b=_b+1;
+    IF _b = _countTotal THEN
+        LEAVE simple_loop;
+    END IF;
+    END LOOP;
+
+    UPDATE certificacion
+    SET
+      control_cotizacion_id = _idCotizacion
+    WHERE id = _datosControidOld;
+
+    UPDATE control
+    SET
+      control_cotizacion_id = _idCotizacion
+    WHERE id = _datosControidOld;
 
     simple_loop: LOOP
     SET _a=_a+1;
